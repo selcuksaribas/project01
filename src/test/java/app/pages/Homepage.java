ff4e10c6d75edd38350f545f7ca270063262ab44
selcuksaribas <141958831+selcuksaribas@users.noreply.github.com>
2023-11-16 17:57:09 -0700

    V4

--- src/test/java/app/pages/Homepage.java
+++ src/test/java/app/pages/Homepage.java
@@ -5,19 +5,33 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class Homepage {
-    public Homepage(){
-        PageFactory.initElements(Driver.getDriver(),this);
+public class HomePage {
+
+    public HomePage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+
     }
-    @FindBy(xpath = "//span[contains(text(),'Sign In')]")public WebElement signInOption;
-    @FindBy(id = "username")public WebElement emailOrUsername;
-    @FindBy(id = "password")public WebElement password;
-    @FindBy(id = "rememberme")public WebElement rememberMeCheckBox;
-    @FindBy(name = "login")public WebElement signInButton;
-    @FindBy(xpath = "//span[contains(text(),'Sign Out')]")public WebElement signOutButton;
-    @FindBy(xpath = "//a[contains(text(),'Confirm and log out')]")public WebElement confirmLogoutButton;
-    @FindBy(xpath = "(//input[@type='search'])[1]")public WebElement searchBox;
-    @FindBy(xpath = "//div[@class='autocomplete-suggestions']//div[.='man boot']")public WebElement elementInAutoSuggestion;
+
+    @FindBy(xpath = "//span[contains(text(),'Sign In')]")
+    public static WebElement signInOption;
+
+    @FindBy(id = "username")
+    public static WebElement emailOrUsername;
+
+    @FindBy(id = "password")
+    public static WebElement password;
+
+    @FindBy(name = "login")
+    public static WebElement signInButton;
+
+    @FindBy(xpath = "(//input[@type='search'])[1]")
+    public static WebElement searchBox;
+
+    @FindBy(xpath = "//div[@class='autocomplete-suggestions']//div[.='man boot']")
+    public static WebElement elementInAutoSuggestion;
+
+    @FindBy(xpath = "//span[contains(text(),'Sign Out')]")
+    public WebElement signOutButton;
 
 
 }
---
+++ src/test/java/app/tests/US_13/class12.java
@@ -0,0 +1,7 @@
+package app.tests.US_13;
+
+public class class12 {
+    public static void main(String[] args) {
+
+    }
+}
