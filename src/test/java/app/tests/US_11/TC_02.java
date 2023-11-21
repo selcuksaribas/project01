9f1acda7465b3782292d6e89f1e23deb2773f7fb
Ilknur S <i.sancaktutar@gmail.com>
2023-11-21 17:58:06 +0000

    user story 11 and 12

---
+++ src/test/java/app/tests/US_11/TC_02.java
@@ -0,0 +1,80 @@
+package app.tests.US_11;
+
+import app.utilities.ConfigReader;
+import app.utilities.Driver;
+import app.utilities.JSUtils;
+import app.utilities.WaitUtils;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.VendorMyAccountPage;
+import pages.VendorSignInPage;
+public class TC_02 {
+
+    @Test
+    public void TC_02() {
+        Driver.getDriver().get(ConfigReader.getProperty("allover_commerce_url"));
+
+       /*
+        Vendor should navigate to "https://allovercommerce.com/"
+Vendor signs in.
+Verify  "dashboard" is visible.
+Verify "store manage"  is visible
+Verify "orders  is visible.
+Verify "downloads" is visible.
+Verify "addresses" is visible.
+Verify "account details" is visible.
+Verify "wishlist" is visible.
+Verify "support tickets" is visible.
+Verify "followings" is visible.
+Verify "logout" is visible.
+        */
+
+        VendorSignInPage vendorSignInPage = new VendorSignInPage();
+        VendorMyAccountPage vendorMyAccountPage = new VendorMyAccountPage();
+
+        vendorSignInPage.signInWidget.click();
+        vendorSignInPage.usernameInput.sendKeys("sblbtl87@gmail.com");
+        vendorSignInPage.passwordInput.sendKeys("Sibelbetul1987");
+        vendorSignInPage.signInButton.click();
+
+
+        JSUtils.JSscrollAllTheWayDown();
+        WaitUtils.waitFor(2);
+        vendorSignInPage.myAccount.click();
+        WaitUtils.waitFor(2);
+
+        Assert.assertTrue(vendorSignInPage.myAccount.isDisplayed());
+        WaitUtils.waitFor(1);
+
+
+
+
+
+
+        Assert.assertTrue(vendorMyAccountPage.dashboard.isDisplayed());
+
+       Assert.assertTrue(vendorMyAccountPage.storeManager.isDisplayed());
+       Assert.assertTrue(vendorMyAccountPage.orders.isDisplayed());
+        Assert.assertTrue(vendorMyAccountPage.downloads.isDisplayed());
+        Assert.assertTrue(vendorMyAccountPage.addresses.isDisplayed());
+        Assert.assertTrue(vendorMyAccountPage.accountDetails.isDisplayed());
+        Assert.assertTrue(vendorMyAccountPage.wishlist.isDisplayed());
+        Assert.assertTrue(vendorMyAccountPage.supportTickets.isDisplayed());
+      Assert.assertTrue(vendorMyAccountPage.followings.isDisplayed());
+        Assert.assertTrue(vendorMyAccountPage.logout.isDisplayed());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
