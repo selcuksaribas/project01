5a0673cd6378f76b8b9b1c93d60694d7f6720b9d
Ismail Sahin <139574740+Kaoshark@users.noreply.github.com>
2023-11-25 18:36:26 -0500

    Add files via upload

---
+++ src/test/java/app/pages/AlloverHomePage.java
@@ -0,0 +1,16 @@
+package myapp.pages;
+
+import myapp.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class AlloverHomePage {
+    public AlloverHomePage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    @FindBy(xpath = "//a[@class='register inline-type']")
+    public WebElement userIcon;
+
+
+}
