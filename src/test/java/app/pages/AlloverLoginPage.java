32139be906cb551d3526921e305ff9fcde5077b1
hallerturk <halilerturk91@gmail.com>
2023-11-25 19:53:49 -0500

    Reports Added, Before and After will be add

---
+++ src/test/java/app/pages/AlloverLoginPage.java
@@ -0,0 +1,27 @@
+package app.pages;
+
+import app.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class AlloverLoginPage {
+    public AlloverLoginPage(){
+
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    @FindBy(id = "reg_username")
+    public WebElement userName;
+
+    @FindBy(id = "reg_email")
+    public WebElement eMail;
+
+    @FindBy(id = "reg_password")
+    public WebElement Password;
+
+    @FindBy(id = "register-policy")
+    public WebElement policyButton;
+
+    @FindBy(xpath = "//button[@class='woocommerce-Button woocommerce-button button woocommerce-form-register__submit']")
+    public WebElement registerButton;
+}
--- src/test/java/app/tests/US_08/Tests_US_08.java
+++ src/test/java/app/tests/US_08/Tests_US_08.java
@@ -5,54 +5,76 @@ import app.utilities.*;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+
 
 //To Do Create @Before and @After annotations (high priority)
-
-
-////////
-//      Extend Reports and Log4J should create (high priority)
-//      sign out may need (low priority)
-//      for parallel testing, tests may need priority or grouping Because first TC_01 done then the other should come (low priority)
+//              sign out may need (low priority)
+//              for parallel testing, tests may need priority or grouping Because first TC_01 done then the other should come (low priority)
 
 public class Tests_US_08 {
+
+/*
+    ADD EXTENT REPORTS CAPABILITY
+    1. use createTestReport(String name,String description) first for creating extent report capability
+    2. then use any method to mark as pass, info, fail
+    3. Then use flush to generate the report
+ */
+
+    /*
+    ADD LOGGER REPORT CAPABILITY
+    1. use any method from LoggerUtils directly.
+     */
+
+    //-------TC_001--STARTS-------//
 //1- User should navigate to "https://allovercommerce.com/"
 //2- User clicks on the Sign-in button at home page
 //3- User enters valid Username or email address
 //4- User enters valid password
 //5- User clicks on the Sign-in button at login page
 //6- User is able to Sign In
+    ////The Steps Above Repeat Before Each @Test
 //7- User clicks on to the 'search box'
 //8- User enter a product name
 //9- User clicks the 'search icon'
 //10- User clicks the 'add to wishlist' icon first product on list
 //11- User must see color of 'add to wishlist' icon changed
-
-
     @Test
     public void TC_01(){
+        LoggerUtils.info("Starting Test US_08 --> TC_01...");
+        //1. use createTestReport(String name,String description) first for creating extent report capability
+        ExtentReportUtils.createTestReport("US_08 Test Report","Test report for TC_01 Login website");
+        //2. then use any method to mark as pass, info, fail
+        ExtentReportUtils.pass("Starting Test US_08 --> TC_01...");
         //Create page object for HomePage
         HomePage homePageHalil = new HomePage();
         //1- User should navigate to "https://allovercommerce.com/"
         Driver.getDriver().get(ConfigReader.getProperty("allover_commerce_url"));
+        ExtentReportUtils.pass("User At The Home Page...");
         //get the page title
         String title = Driver.getDriver().getTitle();
         Assert.assertTrue(title.contains(ConfigReader.getProperty("allover_commerce_title")));
         //2- User clicks on the Sign-in button at home page
         homePageHalil.signInOption.click();
+        ExtentReportUtils.info("User Name is : haliltestng and Password is : *****");
         //3- User enters valid Username or email address ==> property=>us_08_username (haliltestng)
         homePageHalil.emailOrUsername.sendKeys("haliltestng");
         //4- User enters valid password ==> property=>us_08_password (testng)
         homePageHalil.password.sendKeys("testng");
+        ExtentReportUtils.passAndCaptureScreenshot("User Name is : haliltestng and Password is : *****");
         //5- User clicks on the Sign-in button at login page
         homePageHalil.rememberMeCheckBox.click();
+        ExtentReportUtils.pass("User Enter Credentials...");
         //6- User is able to Sign In
         homePageHalil.signInButton.click();
         //wait may need if the internet connection is slow or website is slow
         Assert.assertTrue(homePageHalil.signOutButton.isDisplayed());
+        ExtentReportUtils.passAndCaptureScreenshot("User Successfully SignIn...");
         //7- User clicks on to the 'search box'
         homePageHalil.searchBox.click();
         //8- User enter a product name ==> apple
         homePageHalil.searchBox.sendKeys("apple");
+        ExtentReportUtils.pass("User Successfully Search...");
         //9- User clicks the 'search icon'
         homePageHalil.searchIcon.click();
         //10- User clicks the 'add to wishlist' icon first product on list
@@ -62,9 +84,14 @@ public class Tests_US_08 {
         WaitUtils.waitFor(3);
         JSUtils.JSscrollIntoView(homePageHalil.searchResult1WishIconAdded);
         ActionsUtil.actionsHoverOverOnElement(homePageHalil.searchResult1WishIconAdded);
+        ExtentReportUtils.passAndCaptureScreenshot("User Successfully Add The Item On Wish List...");
         Assert.assertTrue(homePageHalil.searchResult1WishIconAdded.isDisplayed());
-
         Driver.closeDriver();
+        ExtentReportUtils.pass("Driver is Closed...Test Completed Successfully...");
+        LoggerUtils.info("Driver is Closed...Test Completed Successfully...");
+
+        //3. Then use flush to generate the report
+        ExtentReportUtils.flush();
     }
 
 //same with TC_01
@@ -75,6 +102,11 @@ public class Tests_US_08 {
 //13- User must see all chosen product on the wishlist page
     @Test
     public void TC_02(){
+        LoggerUtils.info("Starting Test US_08 --> TC_02...");
+        //1. use createTestReport(String name,String description) first for creating extent report capability
+        ExtentReportUtils.createTestReport("US_08 Test Report","Test report for TC_02 Login website");
+        //2. then use any method to mark as pass, info, fail
+        ExtentReportUtils.pass("Starting Test US_08 --> TC_02...");
         //Create page object for HomePage
         HomePage homePageHalil = new HomePage();
         WaitUtils.waitFor(3);
@@ -114,9 +146,15 @@ public class Tests_US_08 {
         ActionsUtil.actionsScrollDown();
         WaitUtils.waitFor(5);
         ActionsUtil.actionsHoverOverOnElement(homePageHalil.wishAddedTableQuickView);
+        ExtentReportUtils.passAndCaptureScreenshot("User Able to Click Quick View");
         Assert.assertTrue(homePageHalil.wishAddedTable.isDisplayed());
 
         Driver.closeDriver();
+        ExtentReportUtils.pass("Driver is Closed...Test Completed Successfully...");
+        LoggerUtils.info("Driver is Closed...Test Completed Successfully...");
+
+        //3. Then use flush to generate the report
+        ExtentReportUtils.flush();
     }
 
 //same with TC_01
@@ -126,6 +164,11 @@ public class Tests_US_08 {
 //15- User able to see the properties of the product for first product
     @Test
     public void TC_03(){
+        LoggerUtils.info("Starting Test US_08 --> TC_03...");
+        //1. use createTestReport(String name,String description) first for creating extent report capability
+        ExtentReportUtils.createTestReport("US_08 Test Report","Test report for TC_03 Login website");
+        //2. then use any method to mark as pass, info, fail
+        ExtentReportUtils.pass("Starting Test US_08 --> TC_03...");
         //Create page object for HomePage
         HomePage homePageHalil = new HomePage();
         //1- User should navigate to "https://allovercommerce.com/"
@@ -155,11 +198,17 @@ public class Tests_US_08 {
         Assert.assertTrue(homePageHalil.wishAddedTable.isDisplayed());
         //14- User clicks first 'QUICK VIEW' button
         homePageHalil.wishAddedTableQuickView.click();
+        ExtentReportUtils.passAndCaptureScreenshot("User Able to See Item Features");
         //15- User able to see the properties of the product for first product
         WaitUtils.waitFor(5);
         Assert.assertTrue(homePageHalil.wishAddedTableQuickViewProperties.isDisplayed());
 
         Driver.closeDriver();
+        ExtentReportUtils.pass("Driver is Closed...Test Completed Successfully...");
+        LoggerUtils.info("Driver is Closed...Test Completed Successfully...");
+
+        //3. Then use flush to generate the report
+        ExtentReportUtils.flush();
     }
 
 //same with TC_01
@@ -187,6 +236,11 @@ public class Tests_US_08 {
 //32- User enters same data (19 to 26)
     @Test
     public void TC_04(){
+        LoggerUtils.info("Starting Test US_08 --> TC_04...");
+        //1. use createTestReport(String name,String description) first for creating extent report capability
+        ExtentReportUtils.createTestReport("US_08 Test Report","Test report for TC_04 Login website");
+        //2. then use any method to mark as pass, info, fail
+        ExtentReportUtils.pass("Starting Test US_08 --> TC_04...");
         //Create page object for HomePage
         HomePage homePageHalil = new HomePage();
         //Create page object for CheckOutPage
@@ -236,41 +290,68 @@ public class Tests_US_08 {
         //18- User fills in billing information
         //19- User enters 'First name' ->	Halil
         checkOutPageHalil.firstName.clear();
+        ExtentReportUtils.info("User Enter firstName: Halil");
         checkOutPageHalil.firstName.sendKeys("Halil");
         //20- User enters 'Last name' ->	TestNG
         checkOutPageHalil.lastName.clear();
+        ExtentReportUtils.info("User Enter lastName: TestNG");
         checkOutPageHalil.lastName.sendKeys("TestNG");
         //21- User enters 'Company name' ->	TechPro Education
         //22- User enters 'Country / Region' ->	Canada
+        ExtentReportUtils.info("User Drop Down Region: Canada");
         BrowserUtils.dropdownSelectByVisibleText(checkOutPageHalil.countryRegionDropdown,"Canada");
         //23- User enters 'Street address' ->	999 Java Street
         checkOutPageHalil.streetAddressFirstLine.clear();
+        ExtentReportUtils.info("User Enter Street Address: 999 Java Street");
         checkOutPageHalil.streetAddressFirstLine.sendKeys("999 Java Street");
         //24- User enters 'Town/City' ->	Toronto
         checkOutPageHalil.townCity.clear();
+        ExtentReportUtils.info("User Enter City: Toronto");
         checkOutPageHalil.townCity.sendKeys("Toronto");
         //25- User enters 'Province' ->	Ontario
+        ExtentReportUtils.info("User Drop Down Province: Ontario");
         BrowserUtils.dropdownSelectByVisibleText(checkOutPageHalil.provinceStateDropdown,"Ontario");
         //26- User enters 'Postal Code' ->	M5V 3L9
         checkOutPageHalil.postCodeZIP.clear();
+        ExtentReportUtils.info("User Enter Post Code: M5V 3L9");
         checkOutPageHalil.postCodeZIP.sendKeys("M5V 3L9");
         //27- User enters 'Phone' ->	4679872143
         checkOutPageHalil.phone.clear();
+        ExtentReportUtils.info("User Enter Phone: 4679872143");
         checkOutPageHalil.phone.sendKeys("4679872143");
         //28- User enters 'Email address' ->	halile.test.ng@gmail.com
         //extra step
+        ExtentReportUtils.info("User Enter Order Note: This purchase for test reasons");
         checkOutPageHalil.orderNote.sendKeys("This purchase for test reasons");
+        ExtentReportUtils.passAndCaptureScreenshot("User Entered Information...");
         //29- User choose "Payment Methods" ->	Wire transfer/EFT' or 'Pay at door'
         WaitUtils.waitFor(1);
         ActionsUtil.actionsScrollDown();
         WaitUtils.waitFor(3);
         checkOutPageHalil.payAtTheDoorNew.click();
+        ExtentReportUtils.pass("User Click Pay At The Door Option...");
         //30- User clicks "PLACE ORDER"
         checkOutPageHalil.placeOrderButton.click();
         //31- see alarm for report
         WaitUtils.waitFor(1);
         Assert.assertTrue(checkOutPageHalil.noShippingMethodAlert.isDisplayed());
+        WaitUtils.waitFor(2);
+        ExtentReportUtils.fail("User Not Able To Complete Purchase...");
+        ExtentReportUtils.passAndCaptureScreenshot("User Not Able To Complete Purchase...");
+        //We want to take a screenshot of failure
+        try {
+            MediaUtils.takeScreenshotOfTheEntirePage();
+            LoggerUtils.warn("User Not Able to Complete Purchase and Screen Shoot Taken By Media Utils");
+        } catch (IOException e) {
+            LoggerUtils.error("User Not Able to Complete Purchase and Screen Shoot Could Not Taken By Media Utils");
+            throw new RuntimeException(e);
+        }
 
         Driver.closeDriver();
+        ExtentReportUtils.pass("Driver is Closed...Test Completed Successfully...");
+        LoggerUtils.info("Driver is Closed...Test Completed Successfully...");
+
+        //3. Then use flush to generate the report
+        ExtentReportUtils.flush();
     }
 }
